d76d8f3cab5aef9092da61b778ddfd7c1018566b
BagasAlterra <bagas.dhitya@alterra.id>
2023-07-26 11:31:25 +0700

    create name for url tab and config themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "seambiosys-orange": "#f25801",
+        "seambiosys-blue": "#012470",
+      },
+    },
   },
   plugins: [],
 };
